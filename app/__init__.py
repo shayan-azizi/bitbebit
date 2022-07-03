929b5a0e74fd0dd1451edc4c2f999a174c6177f5
Shayan Azizi <80700762+shayan-azizi@users.noreply.github.com>
2022-07-03 15:05:18 +0430

    Create database and __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from app.extensions import db , csrf
+from app.views import views
+from app.auth import auth
+
+
+def create_app():
+    global db
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = "asdsada"
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+    app.register_blueprint(views , url_prefix = "")
+    app.register_blueprint(auth , url_prefix = "")
+
+    db.init_app(app=app)
+    csrf.init_app(app=app)
+
+    with app.app_context():
+        db.create_all()    
+        
+    return app
