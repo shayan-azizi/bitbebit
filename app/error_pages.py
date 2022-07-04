358d5df0182a6d91692b74638b944e3920cb8d2a
Shayan Azizi <80700762+shayan-azizi@users.noreply.github.com>
2022-07-04 19:21:40 +0430

    Add 404 page and some content to login page

--- app/error_pages.py
+++ app/error_pages.py
@@ -1,6 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
 errors = Blueprint("errors" , __name__ , template_folder="templates" , static_folder="static")
 
 
+@errors.errorhandler(404)
+def page_not_found (e):
+    return render_template("404.html"), 404
+    
